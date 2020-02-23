e812984dbf3500a0b7182b65c2b879f723c937c4
cn196097 <405-cn196097@users.noreply.www-apps.univ-lehavre.fr>
2020-02-23 19:39:24 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,13 +2,7 @@
 
 $_ENV['sgbd'] = 'mysql';
 $_ENV['host'] = "localhost";
-$_ENV['user'] = "noel";
-$_ENV['db'] = "projetNoel";
-$_ENV['password'] = "cn196097";
-
-//$_ENV['sgbd'] = 'pgsql';
-//$_ENV['host'] = "localhost";
-//$_ENV['user'] = "postgres";
-//$_ENV['db'] = "projetnoel";
-//$_ENV['password'] = "dile";
+$_ENV['user'] = "";
+$_ENV['db'] = "";
+$_ENV['password'] = "";
 
