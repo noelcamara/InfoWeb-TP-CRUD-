40b816d3ae264d9a1b5f54fbf78596990b533b8c
pt151384 <thomas.peronny@etu.univ-lehavre.fr>
2020-02-24 23:37:02 +0100

    'v2'

--- config.php
+++ config.php
@@ -7,9 +7,10 @@ $_ENV['db'] = "test";
 $_ENV['password'] = "oui";
 
 /* university */
+/*
 $_ENV['sgbd'] = 'mysql';
 $_ENV['host'] = "ust-infoserv.univlehavre.lan";
 $_ENV['user'] = "pt151384";
 $_ENV['db'] = "pt151384";
 $_ENV['password'] = "20142013";
-
+*/
